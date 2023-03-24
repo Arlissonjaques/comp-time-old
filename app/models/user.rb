78ac19ff7a13a62b020384074a49f97878246a77
Arlisson Jaques <arlissonjaques@gmail.com>
2023-03-23 22:49:09 -0300

    refactoring of the from_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,12 +18,9 @@ class User < ApplicationRecord
   has_many :organizations, through: :user_organizations
 
   def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
-
-    user ||= User.create(email: data['email'],
-                        password: Devise.friendly_token[0, 20])
-    user
-  end
+    email = access_token.info['email']
+    User.find_or_create_by(email: email) do |user|
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end  
 end
-# annual leave
